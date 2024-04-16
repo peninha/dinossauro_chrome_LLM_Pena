06628e73d353f4a083e5040bfa2923c8a3b07a16
Inteligência Mil Grau <48864772+inteligenciamilgrau@users.noreply.github.com>
2024-04-15 23:35:44 -0300

    Arquivos para o campeonato de IAs

---
+++ README.md
@@ -0,0 +1,56 @@
+<p align="center">
+  <img src="https://github.com/dhhruv/Chrome-Dino-Runner/blob/master/assets/DinoWallpaper.png" width="97" height="97">
+  <h2 align="center" style="margin-top: -4px !important;">Campeonato de Dino com ChatGPT 😂😂</h2>
+</p>
+
+### Introdução:
+
+-	O Dinosaur Game, também conhecido como **T-Rex Game, Steve the Jumping Dinosaur ou Dino Runner** e inicialmente codinome Project Bolan, é um jogo de navegador integrado no **Google Chrome Web Browser**. O jogo foi criado por **Sebastien Gabriel em 2014** e pode ser acessado pressionando a barra de espaço no modo offline no Google Chrome.
+
+### Sobre:
+
+-	A seguir, representamos uma versão recriada para LLM jogar do famoso Dinosaur Game no modo off-line do navegador Chrome, implementado usando **Python e PyGame**. A adaptção foi feita por Dhruv Panchal e pode ser baixada no (https://github.com/dhhruv/Chrome-Dino-Runner)[https://github.com/dhhruv/Chrome-Dino-Runner]. O arquivo do projeto contém **Arquivos de imagem** e um script python **(chromedino_V1_GPT4.py)**.
+-	Uma GUI simples e fácil de usar é fornecida para uma melhor jogabilidade. O design da jogabilidade é tão simples que o usuário não achará difícil de usar e entender. Diferentes imagens são usadas no desenvolvimento deste projeto de jogo simples, o ambiente de jogo é igual ao jogo Chrome Dino Run original. Para demonstração do projeto, dê uma olhada no GIF abaixo.
+
+<p align="center">
+  <img src="https://github.com/dhhruv/Chrome-Dino-Runner/blob/master/assets/Other/Chrome%20Dino.gif">
+</p>
+
+### Instalação:
+
+-	Antes de mais nada, **clone o repositorio** com:
+```
+git clone https://github.com/dhhruv/Chrome-Dino-Runner.git
+``` 
+**OU**
+Faça o Download do ZIP e descompacte seu conteudo.
+
+-	Depois instale as dependencias pelo Command Prompt OU Terminal usando:
+```
+pip install requirements.txt
+```
+
+### Jogar:
+
+-	Depois da instalação, configure sua API da OpenAI na pasta ".env", e rode o [`chromedino_V1_GPT4.py`] com
+```
+python chromedino.py
+```
+
+### Importante:
+
+-   A velocidade foi diminuida para dar tempo para a LLM pensar!
+-   A variável "jogar" tem o prompt, use ele para melhorar a performance!
+-   Você pode observar 3 variaveis:
+-   O tipo de inimigo que é Cactus ou Bird
+-   A distancia que ele está de você
+-   A altura que ele tem
+
+### PS:
+
+-   Agradeço demais Dhruv Panchal que fez essa versão para Python porque todas outras versões que encontrei, nenhuma tinha o pássaro. Ou quando tinha, o Dino não abaixava. Sem falar nas nuvens e no cenário que no geral ou não tinha, ou era bugado!
+
+### References:
+-	http://www.pygame.org/docs
+-	https://en.wikipedia.org/wiki/Dinosaur_Game
+-	Various articles and videos.
---
+++ chromedino_V1_GPT4.py
@@ -0,0 +1,466 @@
+# !/usr/bin/python
+# -*- coding: utf-8 -*-
+import datetime
+import json
+import os
+import random
+import threading
+import time
+from dotenv import load_dotenv
+load_dotenv()
+import pygame
+from openai import OpenAI
+
+#modelo = "gpt-3.5-turbo-0125"
+modelo = "gpt-4-1106-preview"
+
+dados = {'inimigo': 'indefinida', 'distancia': "indefinida", 'altura': 'indefinida'}
+acao = {"acao": "nenhuma"}
+jogar = ""
+
+def atualiza(dados):
+    global jogar
+    jogar = f"""Você é um dinossauro e está em uma aventura. Você tem a altura de <sua altura>120</sua altura>.
+    Você vai receber dados sobre o ambiente e precisa tomar uma decisão.
+    No ambiente há dois inimigos: cactus e birds.
+
+    Baseado nos dados do inimigo, distancia e na altura dele decida se pula, abaixa ou não faz nada.
+
+    Dados do inimigo:
+    '''
+    Tipo de Inimigo: {dados["inimigo"]}
+    Distancia do Inimigo: {dados["distancia"]}
+    <altura do inimigo>{dados["altura"]}</altura do inimigo>
+    '''
+
+    Dicas:
+    '''
+    Se a altura do Bird for maior que a sua, abaixe.
+    Se a altura do Bird for menor, pule.
+    Compare a sua altura, <sua altura>120</sua altura>, com a <altura do inimigo>{dados["altura"]}</altura do inimigo> que recebeu nos dados.
+
+    Cactos podem enroscar no seu pé, pule.
+    Porém só pule ou abaixe se a 'distancia' estiver entre 300 e 540.
+    '''
+
+    responda em JSON como nos exemplos.
+    "acao": "pular", "distancia do inimigo": "..."
+    "acao": "abaixar", "distancia do inimigo": "..."
+    "acao": "nenhuma", "distancia do inimigo": "..."
+    """
+    return jogar
+
+pygame.init()
+
+# Global Constants
+
+SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 1100
+SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+pygame.display.set_caption("Chrome Dino Runner")
+
+Ico = pygame.image.load("assets/DinoWallpaper.png")
+pygame.display.set_icon(Ico)
+
+RUNNING = [
+    pygame.image.load(os.path.join("assets/Dino", "DinoRun1.png")),
+    pygame.image.load(os.path.join("assets/Dino", "DinoRun2.png")),
+]
+JUMPING = pygame.image.load(os.path.join("assets/Dino", "DinoJump.png"))
+DUCKING = [
+    pygame.image.load(os.path.join("assets/Dino", "DinoDuck1.png")),
+    pygame.image.load(os.path.join("assets/Dino", "DinoDuck2.png")),
+]
+
+SMALL_CACTUS = [
+    pygame.image.load(os.path.join("assets/Cactus", "SmallCactus1.png")),
+    pygame.image.load(os.path.join("assets/Cactus", "SmallCactus2.png")),
+    pygame.image.load(os.path.join("assets/Cactus", "SmallCactus3.png")),
+]
+LARGE_CACTUS = [
+    pygame.image.load(os.path.join("assets/Cactus", "LargeCactus1.png")),
+    pygame.image.load(os.path.join("assets/Cactus", "LargeCactus2.png")),
+    pygame.image.load(os.path.join("assets/Cactus", "LargeCactus3.png")),
+]
+
+BIRD = [
+    pygame.image.load(os.path.join("assets/Bird", "Bird1.png")),
+    pygame.image.load(os.path.join("assets/Bird", "Bird2.png")),
+]
+
+CLOUD = pygame.image.load(os.path.join("assets/Other", "Cloud.png"))
+
+BG = pygame.image.load(os.path.join("assets/Other", "Track.png"))
+
+FONT_COLOR=(0,0,0)
+
+class Dinosaur:
+
+    X_POS = 80
+    Y_POS = 310
+    Y_POS_DUCK = 340
+    JUMP_VEL = 8.5
+
+    def __init__(self):
+        self.duck_img = DUCKING
+        self.run_img = RUNNING
+        self.jump_img = JUMPING
+
+        self.dino_duck = False
+        self.dino_run = True
+        self.dino_jump = False
+
+        self.step_index = 0
+        self.jump_vel = self.JUMP_VEL
+        self.image = self.run_img[0]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.X_POS
+        self.dino_rect.y = self.Y_POS
+
+    def update(self, userInput):
+        global acao
+        agir = acao["acao"]
+        if self.dino_duck:
+            self.duck()
+        if self.dino_run:
+            self.run()
+        if self.dino_jump:
+            self.jump()
+
+        if self.step_index >= 10:
+            self.step_index = 0
+
+        if (userInput[pygame.K_UP] or userInput[pygame.K_SPACE] or agir == "pular") and not self.dino_jump:
+            self.dino_duck = False
+            self.dino_run = False
+            self.dino_jump = True
+        elif (userInput[pygame.K_DOWN] or agir == "abaixar" ) and not self.dino_jump:
+            self.dino_duck = True
+            self.dino_run = False
+            self.dino_jump = False
+        elif not (self.dino_jump or userInput[pygame.K_DOWN]):
+            self.dino_duck = False
+            self.dino_run = True
+            self.dino_jump = False
+
+    def duck(self):
+        self.image = self.duck_img[self.step_index // 5]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.X_POS
+        self.dino_rect.y = self.Y_POS_DUCK
+        self.step_index += 1
+
+    def run(self):
+        self.image = self.run_img[self.step_index // 5]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.X_POS
+        self.dino_rect.y = self.Y_POS
+        self.step_index += 1
+
+    def jump(self):
+        self.image = self.jump_img
+        if self.dino_jump:
+            self.dino_rect.y -= self.jump_vel * 4
+            self.jump_vel -= 0.8
+        if self.jump_vel < -self.JUMP_VEL:
+            self.dino_jump = False
+            self.jump_vel = self.JUMP_VEL
+
+    def draw(self, SCREEN):
+        SCREEN.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
+
+
+class Cloud:
+    def __init__(self):
+        self.x = SCREEN_WIDTH + random.randint(800, 1000)
+        self.y = random.randint(50, 100)
+        self.image = CLOUD
+        self.width = self.image.get_width()
+
+    def update(self):
+        self.x -= game_speed
+        if self.x < -self.width:
+            self.x = SCREEN_WIDTH + random.randint(2500, 3000)
+            self.y = random.randint(50, 100)
+
+    def draw(self, SCREEN):
+        SCREEN.blit(self.image, (self.x, self.y))
+
+
+class Obstacle:
+    def __init__(self, image, type):
+        self.image = image
+        self.type = type
+        self.rect = self.image[self.type].get_rect()
+        self.rect.x = SCREEN_WIDTH
+
+    def update(self):
+        self.rect.x -= game_speed
+        if self.rect.x < -self.rect.width:
+            obstacles.pop()
+
+    def draw(self, SCREEN):
+        SCREEN.blit(self.image[self.type], self.rect)
+
+
+class SmallCactus(Obstacle):
+    def __init__(self, image):
+        self.type = random.randint(0, 2)
+        super().__init__(image, self.type)
+        self.rect.y = 325
+        self.name = "Small Cactus"
+
+
+class LargeCactus(Obstacle):
+    def __init__(self, image):
+        self.type = random.randint(0, 2)
+        super().__init__(image, self.type)
+        self.rect.y = 300
+        self.name = "Large Cactus"
+
+
+class Bird(Obstacle):
+    BIRD_HEIGHTS = [250, 290, 320]
+
+    def __init__(self, image):
+        self.type = 0
+        super().__init__(image, self.type)
+        self.rect.y = random.choice(self.BIRD_HEIGHTS)
+        self.index = 0
+        self.name = "Bird"
+
+    def draw(self, SCREEN):
+        if self.index >= 9:
+            self.index = 0
+        SCREEN.blit(self.image[self.index // 5], self.rect)
+        self.index += 1
+
+
+def main():
+    global game_speed, x_pos_bg, y_pos_bg, points, obstacles, dados, acao
+    run = True
+    clock = pygame.time.Clock()
+    player = Dinosaur()
+    cloud = Cloud()
+    game_speed = 20
+    x_pos_bg = 0
+    y_pos_bg = 380
+    points = 0
+    font = pygame.font.Font("freesansbold.ttf", 20)
+    obstacles = []
+    death_count = 0
+    pause = False
+
+    def score():
+        global points, game_speed
+        points += 1
+        if points % 100 == 0:
+            game_speed += 1
+        current_time = datetime.datetime.now().hour
+
+        file_path = "score.txt"
+        if not os.path.isfile(file_path):
+            # If the file does not exist, create it with some initial content
+            with open(file_path, "w") as f:
+                # Write some initial content to the file
+                f.write("Initial content if needed\n")
+        with open(file_path, "r") as f:
+            score_ints = [int(x) for x in f.read().split()]  
+            highscore = max(score_ints)
+            if points > highscore:
+                highscore=points 
+            text = font.render("High Score: "+ str(highscore) + "  Points: " + str(points), True, FONT_COLOR)
+        textRect = text.get_rect()
+        textRect.center = (900, 40)
+        SCREEN.blit(text, textRect)
+
+    def background():
+        global x_pos_bg, y_pos_bg
+        image_width = BG.get_width()
+        SCREEN.blit(BG, (x_pos_bg, y_pos_bg))
+        SCREEN.blit(BG, (image_width + x_pos_bg, y_pos_bg))
+        if x_pos_bg <= -image_width:
+            SCREEN.blit(BG, (image_width + x_pos_bg, y_pos_bg))
+            x_pos_bg = 0
+        x_pos_bg -= game_speed
+
+    def unpause():
+        nonlocal pause, run
+        pause = False
+        run = True
+
+    def paused():
+        nonlocal pause
+        pause = True
+        font = pygame.font.Font("freesansbold.ttf", 30)
+        text = font.render("Game Paused, Press 'u' to Unpause", True, FONT_COLOR)
+        textRect = text.get_rect()
+        textRect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT  // 3)
+        SCREEN.blit(text, textRect)
+        pygame.display.update()
+
+        while pause:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_u:
+                    unpause()
+
+    frames = 0
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_p:
+                run = False
+                paused()
+
+        current_time = datetime.datetime.now().hour
+        if 7 < current_time < 19:
+            SCREEN.fill((255, 255, 255))
+        else:
+            SCREEN.fill((0, 0, 0))
+        userInput = pygame.key.get_pressed()
+
+        player.draw(SCREEN)
+        player.update(userInput)
+
+        if len(obstacles) == 0:
+            if random.randint(0, 2) == 0:
+                obstacles.append(SmallCactus(SMALL_CACTUS))
+            elif random.randint(0, 2) == 1:
+                obstacles.append(LargeCactus(LARGE_CACTUS))
+            elif random.randint(0, 2) == 2:
+                obstacles.append(Bird(BIRD))
+
+        jogar = 2
+        for obstacle in obstacles:
+            if frames == jogar:
+                dados = {"inimigo": obstacle.name, "distancia": obstacle.rect[0], "altura": (SCREEN_HEIGHT - obstacle.rect[1]) - (SCREEN_HEIGHT - (310 + 94))}
+            obstacle.draw(SCREEN)
+            obstacle.update()
+            if player.dino_rect.colliderect(obstacle.rect):
+                pygame.time.delay(1000)
+                death_count += 1
+                menu(death_count)
+
+        if frames == jogar:
+            frames = 0
+        else:
+            frames += 1
+
+        background()
+
+        cloud.draw(SCREEN)
+        cloud.update()
+
+        score()
+
+        clock.tick(5)
+        pygame.display.update()
+
+
+def menu(death_count):
+    global points
+    global FONT_COLOR
+    global run_agent, dados
+
+    run = True
+    while run:
+        current_time = datetime.datetime.now().hour
+        if 7 < current_time < 19:
+            FONT_COLOR=(0,0,0)
+            SCREEN.fill((255, 255, 255))
+        else:
+            FONT_COLOR=(255,255,255)
+            SCREEN.fill((128, 128, 128))
+        font = pygame.font.Font("freesansbold.ttf", 30)
+
+        if death_count == 0:
+            text = font.render("Press any Key to Start", True, FONT_COLOR)
+        elif death_count > 0:
+            dados = {'inimigo': 'indefinida', 'distancia': "indefinida", 'altura': 'indefinida'}
+            text = font.render("Press any Key to Restart", True, FONT_COLOR)
+            score = font.render("Your Score: " + str(points), True, FONT_COLOR)
+            scoreRect = score.get_rect()
+            scoreRect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 + 50)
+            SCREEN.blit(score, scoreRect)
+            f = open("score.txt", "a")
+            f.write(str(points) + "\n")
+            f.close()
+            with open("score.txt", "r") as f:
+                score = (
+                    f.read()
+                )  # Read all file in case values are not on a single line
+                score_ints = [int(x) for x in score.split()]  # Convert strings to ints
+            highscore = max(score_ints)  # sum all elements of the list
+            hs_score_text = font.render(
+                "High Score : " + str(highscore), True, FONT_COLOR
+            )
+            hs_score_rect = hs_score_text.get_rect()
+            hs_score_rect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 + 100)
+            SCREEN.blit(hs_score_text, hs_score_rect)
+        textRect = text.get_rect()
+        textRect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+        SCREEN.blit(text, textRect)
+        SCREEN.blit(RUNNING[0], (SCREEN_WIDTH // 2 - 20, SCREEN_HEIGHT // 2 - 140))
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                run_agent = False
+                pygame.display.quit()
+                pygame.quit()
+                exit()
+            if event.type == pygame.KEYDOWN:
+                main()
+
+
+
+client = OpenAI()
+
+def generate_answer(messages, model="gpt-3.5-turbo-1106"):
+    try:
+        response = client.chat.completions.create(
+            model=model,
+            messages=[{"role": "user", "content": messages}],
+            temperature=0,
+            seed=42,
+            response_format={"type": "json_object"},
+        )
+
+        return response.choices[0].message.content
+    except Exception as e:
+        print("Erro", e)
+        return e
+
+run_agent = True
+
+def recebe_estados():
+    global run_agent, dados, acao, jogar
+    clock = pygame.time.Clock()
+    while run_agent:
+
+        jogar = atualiza(dados)
+
+        if not dados["inimigo"] == "indefinida":
+            print(60 * "#")
+            print("Dados", dados)
+            #resposta = generate_answer(jogar)
+            resposta = generate_answer(jogar, model="gpt-4-turbo-preview")
+            print("Resp", resposta)
+            acao["acao"] = json.loads(resposta)["acao"]
+            print(acao)
+            time.sleep(0.2)
+        else:
+            time.sleep(1)
+
+t2 = threading.Thread(target=recebe_estados)
+t2.start()
+
+t1 = threading.Thread(target=menu(death_count=0), daemon=True)
+t1.start()
+
+
